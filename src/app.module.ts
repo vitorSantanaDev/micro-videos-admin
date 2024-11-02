da49bec55c5bae21459d0a5fe61a71b7ec23a239
vitorSantanaDev <vitorsantana.developer@gmail.com>
2024-11-02 11:15:43 -0300

    fix: fix import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from './nest-modules/config-module/config.module'
 import { DatabaseModule } from './nest-modules/database-module/database.module'
 import { CategoriesModule } from './nest-modules/categories-module/categories.module'
-import { SharedModule } from './shared/shared.module';
+import { SharedModule } from './nest-modules/shared-module/shared.module'
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule, SharedModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    CategoriesModule,
+    SharedModule
+  ],
   controllers: [],
   providers: []
 })
